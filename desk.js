9e8bef98a113d3faca774385ef030be490a58de2
Prajcurry091307 <77421995+Prajcurry091307@users.noreply.github.com>
2021-12-04 09:46:35 -0800

    Add files via upload

--- desk.js
+++ desk.js
@@ -8,31 +8,38 @@ function preload()
 
 function setup()
 {
-    canvas = createCanvas(640,420);
+    canvas = createCanvas(380,380);
     canvas.center();
     objectDetector=ml5.objectDetector('cocossd',modelLoaded); 
+    document.getElementById("status").innerHTML = "Status : Detecting objects";
+
 }
 function modelLoaded()
 {
     console.log("Model Loaded")
     status = true;
-    objectDetector.detect(img, gotResults);
+    //objectDetector.detect(video, gotResults);
 }
 function draw()
 {
-    image(img,0,0,640,420);
+    image(img,0,0,380,380);
     
     if (status != "")
     {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video , gotResults);
       for (i=0; i <objects.length; i++)
       {
           document.getElementById("status").innerHTML = "Status : Object Detected";
+          document.getElementById("number_of_objects").innerHTML = "Number of objects deteced are: " + objects.length;
 
-          fill("#ff0000");
+          fill(r,g,b);
           percent = floor(objects[i].confidence *100);
           text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 20 );
           noFill();
-          stroke("#ff0000");
+          stroke(r,g,b);
           rect(objects[i].x, objects[i].y,objects[i].width, objects[i].height);
       }
     }
