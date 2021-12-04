1cef6ee06d11d93b2bff7457f9a317327d830bde
Prajcurry091307 <77421995+Prajcurry091307@users.noreply.github.com>
2021-12-04 08:58:35 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+/*img="";
+var status="";   
+function preload()
+{
+    img = loadImage('desk.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded); 
+}
+function modelLoaded()
+{
+    console.log("Model Loaded")
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+     
+
+function gotResults(error, results)
+{
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+}
